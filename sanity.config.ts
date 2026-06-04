bbec92e0bc07ce6b76bc1efee91e2456ff96df71
Emarat <emarat-web@reddashmedia.com>
2026-06-04 11:47:03 +0530

    Hardcoding the project ID as fallback

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   name: "default",
   title: "Emarat CMS",
 
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "k6lgt7ii",
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
 
   plugins: [structureTool(), visionTool()],
